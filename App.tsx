7eae93b8167726e1a017833d27fa733bde5bf146
lrafanan <lrafanan1@csub.edu>
2026-02-26 14:38:59 -0800

    trying to set up stuff for navigation to work but somethings wrong

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import CreateAccount from "./Screens/CreateAccount"; // ignore casing problem if it shows up
-import LogIn from "./Screens/LogIn"; // ignore casing problem if it shows up
+import AppNavigation from './Navigation';
 
 export default function App() {
   return (
-    //<CreateAccount/>
-    <LogIn/>
+    <AppNavigation/>
   );
 }
 
