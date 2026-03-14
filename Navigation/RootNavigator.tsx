b9c926e54838619b9cbf002e99c6950de953b92f
lrafanan <lrafanan1@csub.edu>
2026-03-13 20:52:20 -0700

    removed home from root stack

--- Navigation/RootNavigator.tsx
+++ Navigation/RootNavigator.tsx
@@ -3,7 +3,6 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
 import CreateAccount from "../Screens/CreateAccount";
 import LogIn from "../Screens/LogIn";
-import Home from "../Screens/Home";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +12,6 @@ export default function RootNavigator() {
             <Stack.Navigator screenOptions={{headerShown: false}} id="" initialRouteName="LogIn">
                 <Stack.Screen name="LogIn" component={LogIn} />
                 <Stack.Screen name="CreateAccount" component={CreateAccount} />
-                <Stack.Screen name="Home" component={Home} />
             </Stack.Navigator>
         </NavigationContainer>
     );
