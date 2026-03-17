ad2475c3d944e288f84c54c64010396b1eae11c5
lrafanan <lrafanan1@csub.edu>
2026-03-16 19:26:50 -0700

    Adding adrian's work

--- Screens/LogIn.tsx
+++ Screens/LogIn.tsx
@@ -1,11 +1,37 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, TextInput, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
+import {Alert, AppState, StyleSheet, View, Text, TextInput, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
+import { supabase } from '../lib/supabase'
+
+AppState.addEventListener('change', (state) => {
+  if (state == 'active') {
+    supabase.auth.startAutoRefresh()
+  } else {
+    supabase.auth.stopAutoRefresh()
+  }
+})
+
 
 export default function LogIn ({navigation}) {
-    const [identifier, setIdentifier] = useState("");
+    const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    
-    const handleLogIn = () => {
+    const [loading, setLoading] = useState(false)
+    const [message, setMessage] = useState('')
+
+    async function handleLogIn() {
+       setLoading(true)
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      })
+      
+      console.log("LOGIN RESULT:", data)
+      console.log("LOGIN ERROR:", error)
+
+      if (error) {
+        Alert.alert(error.message)
+        setMessage("ERROR IN LOGGING IN")
+      }
+      setLoading(false)
       
     }
 
@@ -19,13 +45,16 @@ export default function LogIn ({navigation}) {
                 <Text style={styles.title}>Log In</Text>
  
                 {/*USERNAME OR EMAIL*/}
-                <TextInput style={styles.input} placeholder='username or email'
-                    value={identifier}
-                    onChangeText={setIdentifier}
+                <TextInput style={styles.input}
+                    placeholder='email@email.com'
+                    value={email}
+                    onChangeText={setEmail}
                 />
 
                 {/*PASSWORD*/}
-                <TextInput style={styles.input} placeholder='password'
+                <TextInput style={styles.input} 
+                    placeholder='password'
+                    secureTextEntry
                     value={password}
                     onChangeText={setPassword}
                 />
